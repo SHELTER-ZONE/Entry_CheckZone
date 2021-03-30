5e26ff3099ba5b9e45e6fc42d421b643636d0b96
Proladon <proladon@gmail.com>
2021-03-30 23:16:11 +0800

    ver 3 init
    using vite

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <p class="text-lg text-black font-semibold">Erin Lindford</p>
+  <p class="font-light text-gray-100">Product Engineer</p>
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
